03b0a45f5afe1f37e6c7bf7766224892fb7176fe
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-09 16:01:04 -0400

    layout

---
+++ src/Components/Layout/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+/**
+ * Layout
+ * MH = Main Header
+ * CT = Content
+ */
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 250px auto;
+  grid-template-rows: 70px auto;
+
+  grid-template-areas:
+    "MH MH"
+    "CT CT";
+
+  height: 100vh;
+  min-width: 315px;
+
+  @media (max-width: 600px) {
+    grid-template-columns: 100%;
+    grid-template-rows: 70px auto;
+
+    grid-template-areas:
+      "MH"
+      "CT";
+  }
+`;
