c10b12cd00c74d6ea3b37c13d55b483172dedf42
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-09 15:28:04 -0400

    include component mainHeader

---
+++ src/Components/MainHeader/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  grid-area: MH;
+  background-color: #cec6ce;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  border-bottom: 1px solid "#CEC6CE";
+`;
+
+export const Welcome = styled.h3``;
+
+export const UserName = styled.span``;
