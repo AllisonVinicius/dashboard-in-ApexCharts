4e09991a57ea41b74c53de0ef42fe8d1607096a5
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-10 12:16:35 -0400

    component graph Barras

--- src/Components/GraphBarras/index.tsx
+++ src/Components/GraphBarras/index.tsx
@@ -1,7 +1,55 @@
 import React from "react";
+import ReactApexChart from "react-apexcharts";
 import { Container } from "./styles";
-const Barras = () => {
-  <Container>ALlison Barras;</Container>;
+
+const GraphBarras = () => {
+  const series = [
+    {
+      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+    },
+  ];
+
+  const options = {
+    chart: {
+      height: 250,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: [
+        "South Korea",
+        "Canada",
+        "United Kingdom",
+        "Netherlands",
+        "Italy",
+        "France",
+        "Japan",
+        "United States",
+        "China",
+        "Germany",
+      ],
+    },
+  };
+
+  return (
+    <Container>
+      {/* <Header>Graph Barras</Header> */}
+      <ReactApexChart
+        options={options}
+        series={series}
+        type="bar"
+        height={350}
+        width={450}
+      />
+    </Container>
+  );
 };
 
-export default Barras;
+export default GraphBarras;
--- src/Components/GraphBarras/styles.ts
+++ src/Components/GraphBarras/styles.ts
@@ -1,6 +1,34 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 80%;
+  min-height: 260px;
+
+  margin: 10px 0;
+
+  background-color: #fff;
+  border-radius: 10px;
+
+  display: flex;
+
+  @media (max-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    height: auto;
+  }
+`;
+export const Header = styled.header`
+  display: flex;
+
+  background-color: #000;
+  > h2 {
+    margin-bottom: 20px;
+    padding-left: 16px;
+  }
+
+  @media (max-width: 1200px) {
+    flex-direction: column;
+  }
 `;
