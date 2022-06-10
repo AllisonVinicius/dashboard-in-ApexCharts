b21783885db3c19d44c920d5ff3332af03318ab1
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-10 12:18:33 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 import React from "react";
-import "./App.css";
+import Dashboard from "./Pages/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Dashboard />;
+};
 
 export default App;
