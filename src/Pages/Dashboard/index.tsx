6da63a350f013e09a492d731d11f0da06523951c
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-10 12:17:24 -0400

    include row and col graph  side by side

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Content from "../../Components/Content";
+import GraphBarras from "../../Components/GraphBarras";
+import GraphScatter from "../../Components/GraphScatter";
 import Header from "../../Components/Header";
 import Layout from "../../Components/Layout";
+import { Col, Row } from "./styles";
 
 const Dashboard = () => (
   <Layout>
     <Header title="Dashboard" subtitle="Desafio Técnico Frontend" />
-    <Content>graficos aqui</Content>
+    <Content>
+      <Row>
+        <Col>
+          <GraphBarras />
+        </Col>
+        <Col>
+          <GraphScatter />
+        </Col>
+      </Row>
+    </Content>
   </Layout>
 );
 
--- src/Pages/Dashboard/styles.ts
+++ src/Pages/Dashboard/styles.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Barras = styled.div``;
+
+export const Row = styled.div`
+  display: flex;
+
+  flex-direction: row;
+`;
+
+export const Col = styled.div`
+  display: flex;
+  width: 100%;
+`;
