bcb2cd62bf5b9d6ecb148546b521e637234663a4
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-12 18:12:42 -0400

    graph scatter

--- src/Components/GraphScatter/index.tsx
+++ src/Components/GraphScatter/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactApexChart from "react-apexcharts";
+import { options } from "../../repositories/scatter";
 import TextHeader from "../TextHeader";
 import { Container } from "./styles";
 
-const GraphScatter = () => (
-  <Container>
-    <TextHeader title="Scatter" />
-    <ReactApexChart
-      options={options}
-      series={series}
-      type="scatter"
-      height={350}
-      width={450}
-    />
-  </Container>
-);
+const GraphScatter = () => {
+  return (
+    <Container>
+      <TextHeader title="Scatter" />
+      <ReactApexChart
+        options={options}
+        series={options.series}
+        type="scatter"
+        height={350}
+        width={450}
+      />
+    </Container>
+  );
+};
 
 export default GraphScatter;
